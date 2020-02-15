361eb436a981ba71ac783344b652b1ea051534b8
KnightMiner <knightminer4@gmail.com>
2020-02-15 17:22:10 -0600

    Fully override bonusStore lib instead of relying on which mod loads first

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,19 +5,24 @@ local mod = {
 	requirements = {},
 }
 
+local PERF_POWER = 7
+local PERF_POWER_MODIFIED = 9
+
 function mod:init()
   -- override CreateEffect to sub power for a bit more power
   local old = CreateEffect
   function CreateEffect(data, ...)
-    if data.power and not data.stock then
+    if data.rewtype == PERF_POWER or (data.power and not data.stock) then
       if math.random(1, 5) == 1 then
         data.cores = 1
         data.power = 0
       else
         data.power = 3
       end
-      data.stock = -2 -- mark so we don't modify this twice
-      data.rewtype = 9 -- compat with bonusStore lib, set to a value that won't get overridden
+      -- mark so we don't modify this twice
+      data.stock = -2
+      -- compat with bonusStore lib, set to a value that won't get overridden
+      data.rewtype = PERF_POWER_MODIFIED
     end
     return old(data, ...)
   end
