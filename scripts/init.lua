a1aeea91c1520df9fd3a456f37a63851d0669d13
KnightMiner <knightminer4@gmail.com>
2020-02-15 19:48:01 -0600

    Add config for chances

--- scripts/init.lua
+++ scripts/init.lua
@@ -7,13 +7,27 @@ local mod = {
 
 local PERF_POWER = 7
 local PERF_POWER_MODIFIED = 9
+local CORE_CHANCE = 5
+
+function mod:metadata()
+  modApi:addGenerationOption(
+    "perfectedRewardCoreChance",
+    "Reactor Core Chance",
+    "Chance of a reactor core to appear as a perfect island reward instead of 3 power.",
+    {
+      value = 5, -- default
+      values = { 0, 4, 5, 10, 20 },
+      strings = { "Disabled", "25%", "20%", "10%", "5%" }
+    }
+  )
+end
 
 function mod:init()
   -- override CreateEffect to sub power for a bit more power
   local old = CreateEffect
   function CreateEffect(data, ...)
     if data.rewtype == PERF_POWER or (data.power and not data.stock) then
-      if math.random(1, 5) == 1 then
+      if CORE_CHANCE ~= 0 and math.random(1, CORE_CHANCE) == 1 then
         data.cores = 1
         data.power = 0
       else
@@ -29,6 +43,7 @@ function mod:init()
 end
 
 function mod:load(options,version)
+  CORE_CHANCE = options.perfectedRewardCoreChance.value
 end
 
 return mod
